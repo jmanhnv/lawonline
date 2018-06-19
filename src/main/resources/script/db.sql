fd77521ba0a528d2b8409bb5ad4702ba5e2286c0
Johny Nguyen <manh.1403@gmail.com>
2018-06-19 21:23:36 +0700

    update config db

--- src/main/resources/script/db.sql
+++ src/main/resources/script/db.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 50637
  Source Host           : localhost:3306
- Source Schema         : db
+ Source Schema         : lawonline
 
  Target Server Type    : MySQL
  Target Server Version : 50637
@@ -20,24 +20,24 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 -- DATABASE
 -- ----------------------------
-DROP DATABASE IF EXISTS `db`;
-CREATE DATABASE db;
-USE db;
+DROP DATABASE IF EXISTS `lawonline`;
+CREATE DATABASE lawonline;
+USE lawonline;
 
 -- ----------------------------
 -- Table structure for judgment
 -- ----------------------------
 DROP TABLE IF EXISTS `judgment`;
-CREATE TABLE `judgment`  (
+CREATE TABLE `judgment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `category_id` int(11) NOT NULL,
-  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `created_date` date NULL DEFAULT NULL,
-  `file_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `file_path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `updated_date` date NULL DEFAULT NULL,
+  `content` longblob NOT NULL,
+  `created_date` date DEFAULT NULL,
+  `file_name` varchar(255) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `updated_date` date DEFAULT NULL,
   `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`id`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 SET FOREIGN_KEY_CHECKS = 1;
